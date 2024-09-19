7d05b2ac83164c13450ff7b6863c2938da876713
Jung_min Choi <fusion_up@naver.com>
2024-09-19 13:18:06 +0900

    add signup page

--- app/src/main/java/com/fuciple0/jjikgo/activities/SignupActivity.kt
+++ app/src/main/java/com/fuciple0/jjikgo/activities/SignupActivity.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.fuciple0.jjikgo.R
+import com.fuciple0.jjikgo.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivitySignupBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
     }
 }
