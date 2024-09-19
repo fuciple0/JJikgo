b3270cce29fd94a177a77c76a681720327388cda
Jung_min Choi <fusion_up@naver.com>
2024-09-19 10:28:11 +0900

    add login page

--- app/src/main/java/com/fuciple0/jjikgo/activities/IntroActivity.kt
+++ app/src/main/java/com/fuciple0/jjikgo/activities/IntroActivity.kt
@@ -1,6 +1,9 @@
 package com.fuciple0.jjikgo.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,12 +13,17 @@ import com.fuciple0.jjikgo.R
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_intro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+        // 일정시간 후(2초)에 자동으로 LoginActivity를 실행하기
+        // Handler().postDelayed({},2000)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        },2000)
+
+
     }
 }
--- app/src/main/java/com/fuciple0/jjikgo/activities/LoginActivity.kt
+++ app/src/main/java/com/fuciple0/jjikgo/activities/LoginActivity.kt
@@ -1,21 +1,38 @@
 package com.fuciple0.jjikgo.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.fuciple0.jjikgo.R
+import com.fuciple0.jjikgo.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        //둘러보기 글씨 클릭으로 Main 화면으로 이동
+        binding.tvGo.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
+
+        // 회원가입 버튼 클릭
+        binding.btnSignup.setOnClickListener {
+            startActivity(Intent(this, SignupActivity::class.java))
+        }
+
+        // 이메일 로그인 버튼
+        binding.btnLoginEmail.setOnClickListener {
+            startActivity(Intent(this, EmailLoginActivity::class.java))
+        }
+
+
     }
 }
