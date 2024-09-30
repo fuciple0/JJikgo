ea1c94cc69bb588a6cb4346a074db8d6b76b4fc4
Jung_min Choi <fusion_up@naver.com>
2024-09-30 17:22:09 +0900

    레트로핏 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,12 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.circleimageview)
+
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.converter.scalars)
+    implementation(libs.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
